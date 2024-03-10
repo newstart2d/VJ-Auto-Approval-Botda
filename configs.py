8d5b9675ea181633eefcc40c37e6fc6a6068fc5c
newstart2d <162543489+newstart2d@users.noreply.github.com>
2024-03-10 16:25:13 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config:
     API_ID = int(getenv("API_ID", "28726816"))
     API_HASH = getenv("API_HASH", "45edf74203ecf6ff14b394a9e47dca34")
     BOT_TOKEN = getenv("BOT_TOKEN", "7168600940:AAEmzXh8luJnN1E7-Hfkye0DmBNd7p8rmT8")
-    FSUB = getenv("FSUB", "born4movies7")
+    FSUB = getenv("FSUB", "awdawdtgd")
     CHID = int(getenv("CHID", "-1001654163482"))
     SUDO = list(map(int, getenv("SUDO", "5521380948").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://m0921594:Rohit44@cluster0.qbze22i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
